6b3bd4a8e231a75b1248d40bb8fe3764a95c3ba2
Schmidt Sascha <ss532@hdm-stuttgart.de>
2023-09-26 19:59:19 +0200

    add summary to frontend

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -53,7 +53,7 @@ const Demo = () => {
             ↵
           </button>
         </form>
-        <div className="flex flex-col gap-1m max-h-60 overflow-y-auto">
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
           {allArticles.map((article, index) => (
             <div key={`link-${index}`} onClick={() => setArticle(article)} className="link_card">
               <div className="copy_btn">
@@ -63,6 +63,29 @@ const Demo = () => {
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p>
+            Well, seems like there was an error..
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">{error?.data?.error}</span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
